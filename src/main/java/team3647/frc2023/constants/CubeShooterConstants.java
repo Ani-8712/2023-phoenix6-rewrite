3ce86b1cb94da5f498bcf0d2bc7b05ee7cb6d00f
ericg4 <eric.g4@outlook.com>
2023-05-24 16:49:38 -0700

    first attempts, everything is different

--- src/main/java/team3647/frc2023/constants/CubeShooterConstants.java
+++ src/main/java/team3647/frc2023/constants/CubeShooterConstants.java
@@ -1,10 +1,13 @@
 package team3647.frc2023.constants;
 
-import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
-import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
-import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+// import com.ctre.phoenix.motorcontrol.NeutralMode;
+// import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+// import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+// import com.ctre.phoenix.motorcontrol.can.TalonFX;
+// import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 public class CubeShooterConstants {
     public static final TalonFX kTopRoller =
@@ -13,8 +16,10 @@ public class CubeShooterConstants {
             new TalonFX(GlobalConstants.CubeShooterIds.kBottomMasterId);
     private static final TalonFXConfiguration kMasterConfig = new TalonFXConfiguration();
 
-    public static final TalonFXInvertType kTopMotorInvert = TalonFXInvertType.Clockwise;
-    public static final TalonFXInvertType kBottomMotorInvert = TalonFXInvertType.Clockwise;
+    // public static final TalonFXInvertType kTopMotorInvert = TalonFXInvertType.Clockwise;
+    public static final boolean kTopMotorInvert = true;
+    // public static final TalonFXInvertType kBottomMotorInvert = TalonFXInvertType.Clockwise;
+    public static final boolean kBottomMotorInvert = true;
 
     // kG at max extension
     public static final double kNominalVoltage = 11.0;
@@ -22,15 +27,17 @@ public class CubeShooterConstants {
     public static final double kMaxCurrent = 20.0;
 
     static {
-        kTopRoller.configAllSettings(kMasterConfig, GlobalConstants.kTimeoutMS);
+        kTopRoller.getConfigurator().apply(kMasterConfig);
+        // kTopRoller.configAllSettings(kMasterConfig, GlobalConstants.kTimeoutMS);
 
-        kTopRoller.setNeutralMode(NeutralMode.Brake);
+        kTopRoller.NeutralMode(NeutralModeValue.Brake);
+
         kTopRoller.enableVoltageCompensation(true);
         kTopRoller.configVoltageCompSaturation(kNominalVoltage, GlobalConstants.kTimeoutMS);
         kTopRoller.configGetStatorCurrentLimit(
                 new StatorCurrentLimitConfiguration(true, kStallCurrent, kMaxCurrent, 5));
 
-        kBottomRoller.setNeutralMode(NeutralMode.Brake);
+        kBottomRoller.setNeutralMode(NeutralModeValue.Brake);
         kBottomRoller.enableVoltageCompensation(true);
         kBottomRoller.configVoltageCompSaturation(kNominalVoltage, GlobalConstants.kTimeoutMS);
         kBottomRoller.configGetStatorCurrentLimit(
